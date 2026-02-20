f9423c94ec6728cff1f09e88e6c3e900dd1fbde7
Silmarill <mrsilmar@gmail.com>
2026-02-20 16:23:00 +0700

    Добавили пару чисел.

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace asdasd
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            Console.WriteLine("Hello123!");
         }
     }
 }
